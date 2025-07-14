f82e6b629319fe764c86e78571b9df5414d13ffd
Prabhu <pvellais@login-gpu.cm.cluster>
2025-07-14 08:18:03 -0400

    pushing latest

---
+++ soda/common/constants.py
@@ -0,0 +1,8 @@
+CPU_OP = "cpu_op"
+CUDA_RUNTIME = "cuda_runtime"
+GPU_MEMCPY = "gpu_memcpy"
+GPU_MEMSET = "gpu_memset"
+KERNEL = "kernel"
+
+DEFAULT_TRACE_DIR = "trace_artifacts"
+DEFAULT_PROFILE_FILENAME = "inference_profile.json"
