249c70c8f3243ec1f9c6cbc4fc9bc0c6b9191730
shreesh <tripathi.shreeshv@gmail.com>
2025-11-11 14:04:46 -0500

    change hf model download location for speed

--- examples/quickstart.sh
+++ examples/quickstart.sh
@@ -2,6 +2,7 @@ if [ ! -f "pyproject.toml" ]; then
     echo "Error: Please run this script from the root directory of the SODA repository."
     exit 1
 fi
+export HF_HOME=${HF_HOME:-/tmp/hf}
 python -m soda.main \
   --model gpt2 \
   --output-dir ./soda_results \
